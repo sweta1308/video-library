f948c6d4e225eba24afb1c6dfee1dc40bae3fec0
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-11-09 12:19:13 +0530

    feat: add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Video } from "./components/Video";
+import { VideoInput } from "./components/VideoInput";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col items-center bg-purple-50">
+      <h1 className="text-center text-[44px] font-extrabold my-[30px] text-purple-700">
+        Video Library
+      </h1>
+      <VideoInput />
+      <Video />
     </div>
   );
 }
