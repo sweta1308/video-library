75486ff90dd05ce543fdd25863e975196ea6e5b1
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-11-09 12:22:52 +0530

    style: add height

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { VideoInput } from "./components/VideoInput";
 
 function App() {
   return (
-    <div className="App flex flex-col items-center bg-purple-50">
+    <div className="App flex flex-col items-center bg-purple-50 min-h-screen">
       <h1 className="text-center text-[44px] font-extrabold my-[30px] text-purple-700">
         Video Library
       </h1>
