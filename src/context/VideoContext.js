6569ccd127a755fdba92ac54f858e5d8fc6ff3fb
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-11-10 11:36:10 +0530

    refactor: made minor fixes

--- src/context/VideoContext.js
+++ src/context/VideoContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useRef, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const VideoContext = createContext();
 
@@ -6,14 +6,15 @@ export const VideoProvider = ({ children }) => {
   const [videoState, setVideoState] = useState({
     videoUrl: "",
     isPlaying: false,
-    duration: 0,
-    currentTime: 0
+    duration: null,
   });
 
-  const videoEl = useRef();
-  const canvasEl = useRef()
+  const [wavesurfer, setWavesurfer] = useState({
+    surfer: null,
+    hasAudio: true,
+  });
 
-  const value = { videoState, setVideoState, videoEl, canvasEl };
+  const value = { videoState, setVideoState, wavesurfer, setWavesurfer };
 
   return (
     <VideoContext.Provider value={value}>{children}</VideoContext.Provider>
